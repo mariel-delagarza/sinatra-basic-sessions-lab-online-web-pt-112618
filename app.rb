79ad75ebe3b0860a18e6de9f2806dec450d8ee32
Mariel Adrianna de la Garza <mariel.code@gmail.com>
2019-03-20 20:35:51 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -1,4 +1,15 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  configure do
+    enable sessions
+    set :session_secret, "w@ffl3s"
+  end
+
+  get '/' do
+    erb :index
+  end
+
+
 end
