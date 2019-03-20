c28e1ddcf5f553f1d65717ba4dc68ee8f35fd5c5
Mariel Adrianna de la Garza <mariel.code@gmail.com>
2019-03-20 20:45:43 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -12,7 +12,7 @@ class App < Sinatra::Base
   end
 
   post '/checkout' do 
-    params[:item] = session[:item]
+    session[:item] = params[:item]
     @session = session 
     erb :checkout 
   end 
